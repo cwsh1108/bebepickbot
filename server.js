9a4bf41d9f88bd5a32288e9b7a1497af5cc9ff0d
cwsh1108 <lchw69@naver.com>
2026-01-09 06:07:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import express from "express";
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("BEBEPICK BOT OK");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
+});
